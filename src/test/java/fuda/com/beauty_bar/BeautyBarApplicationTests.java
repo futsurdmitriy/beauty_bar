180bb49b90c0fbe8cabd4512b3e665effb70602b
fuda <futsurdmitriy@gmail.com>
2020-08-15 20:24:06 +0300

    Beauty bar first commit.

---
+++ src/test/java/fuda/com/beauty_bar/BeautyBarApplicationTests.java
@@ -0,0 +1,13 @@
+package fuda.com.beauty_bar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautyBarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
